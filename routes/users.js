2c926740674239f12c5095ca7d18e860a9c5822d
Sheldrick <mrmayrant@yahoo.com>
2019-07-04 20:22:52 -0400

    added the user model and validation from express validator

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
 
 // register a user
-router.post("/", (req, res) => {
-	res.send("Register a user");
-});
+router.post(
+	"/",
+	[
+		check("name", "Please add a name").not().isEmpty(),
+		check("email", "Please include a valid email address").isEmail(),
+		check("password", "Please ensure your password has 6 or more characters").isLength({ min: 6 })
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		// if errors isn't empty, send a 400 status and the array of errors
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		res.send("passed");
+	}
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // connect DB
 connectDB();
 
+// init middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.json({ msg: "Welcome to the Contact Keeper API" }));
 
 app.use("/api/auth", require("./routes/auth"));
