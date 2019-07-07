9e9eade5db2226586bfa39fccc1b3bb302504b30
Sheldrick <mrmayrant@yahoo.com>
2019-07-07 08:44:51 -0400

    created the register and login components

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Navbar from "./components/layout/Navbar";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
 import "./App.css";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 const App = () => {
 	return (
@@ -19,6 +21,8 @@ const App = () => {
 							<Switch>
 								<Route exact path="/" component={Home} />
 								<Route exact path="/about" component={About} />
+								<Route exact path="/register" component={Register} />
+								<Route exact path="/login" component={Login} />
 							</Switch>
 						</div>
 					</Fragment>
