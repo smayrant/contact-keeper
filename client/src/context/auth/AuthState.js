e86ec6489472758fb7c720b155436a5b5806811e
Sheldrick <mrmayrant@yahoo.com>
2019-07-08 01:27:50 -0400

    added the ability to logout the user out

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -80,7 +80,7 @@ const AuthState = props => {
 			});
 		}
 	};
-	const logout = () => console.log("logout");
+	const logout = () => dispatch({ type: LOGOUT });
 
 	const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
