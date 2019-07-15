fcd1912ed915855605708a2a2db5f8b79f920d39
Sheldrick <mrmayrant@yahoo.com>
2019-07-15 18:14:30 -0400

    testing for build errors

--- server.js
+++ server.js
@@ -4,25 +4,25 @@ const path = require("path");
 
 const app = express();
 
-// connect DB
+// Connect Database
 connectDB();
 
-// init middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-app.use("/api/auth", require("./routes/auth"));
+// Define Routes
 app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-	// set static folder
+	// Set static folder
 	app.use(express.static("client/build"));
 
 	app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")));
 }
 
-// port is set to an environment variable for production or 5000
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
