36038e2cf37fb6dcbd8605b612e1a7865f59d447
Sheldrick <mrmayrant@yahoo.com>
2019-07-15 16:29:28 -0400

    prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -12,6 +13,14 @@ app.use(express.json({ extended: false }));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+	// set static folder
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")));
+}
 
 // port is set to an environment variable for production or 5000
 const PORT = process.env.PORT || 5000;
